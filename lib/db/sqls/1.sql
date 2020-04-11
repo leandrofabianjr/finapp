4502cc5c07b6990efce86c35afde7dc2907ccba5
leandrofabianjr <leandrofabianjr@gmail.com>
2020-04-11 19:13:46 -0300

    Criado scripts da versão 1 do banco de dados

---
+++ lib/db/sqls/1.sql
@@ -0,0 +1,44 @@
+create table category_type (
+  id integer primary key autoincrement,
+  name text not null,
+  description integer,
+  created_at timestamp not null default current_timestamp,
+  deleted_at timestamp
+);
+
+create table category (
+  id integer primary key autoincrement,
+  name text not null,
+  description text,
+  id_category_type integer not null constraint category_category_type_id_fk references category_type on update restrict on delete restrict,
+  created_at timestamp not null default current_timestamp,
+  deleted_at timestamp
+);
+
+create table account_type (
+  id integer primary key autoincrement,
+  name text not null,
+  description text,
+  created_at timestamp not null default current_timestamp,
+  deleted_at timestamp
+);
+
+create table account (
+  id integer primary key autoincrement,
+  name text not null,
+  description text,
+  id_account_type integer not null references account_type on update restrict on delete restrict,
+  created_at timestamp not null default current_timestamp,
+  deleted_at timestamp
+);
+
+create table movement (
+  id integer primary key autoincrement,
+  name integer not null,
+  description text,
+  data timestamp not null,
+  id_account integer not null references account on update restrict on delete restrict,
+  id_category integer not null references category on update restrict on delete restrict,
+  created_at timestamp not null default current_timestamp,
+  deleted_at timestamp
+);
