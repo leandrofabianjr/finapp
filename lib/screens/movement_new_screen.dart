34bbf97fe1916d258efa395c8ba516a74a5c0d45
leandrofabianjr <leandrofabianjr@gmail.com>
2020-04-12 16:16:26 -0300

    Alterado tela de cadastro de movimentação

--- lib/screens/movement_new_screen.dart
+++ lib/screens/movement_new_screen.dart
@@ -1,4 +1,6 @@
+import 'package:finapp/models/category.dart';
 import 'package:finapp/models/movement.dart';
+import 'package:finapp/screens/category_select.dart';
 import 'package:flutter/material.dart';
 
 class MovementNewScreen extends StatelessWidget {
@@ -19,6 +21,7 @@ class MovementNewForm extends StatefulWidget {
 class _MovementNewFormState extends State<MovementNewForm> {
   final _formKey = GlobalKey<FormState>();
   Movement _movement = Movement();
+  var _txtFieldCategoryCtrl = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -40,6 +43,25 @@ class _MovementNewFormState extends State<MovementNewForm> {
                 return null;
               },
               onSaved: (val) => _movement.name = val,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Categoria',
+              ),
+              controller: _txtFieldCategoryCtrl,
+              readOnly: true,
+              onTap: () {
+                Navigator
+                  .push<Category>(
+                    context,
+                    MaterialPageRoute(builder: (BuildContext context) => CategorySelect())
+                  ).then((cat) {
+                    if (cat != null) {
+                      _movement.idCategory = cat.id;
+                      _txtFieldCategoryCtrl.text = cat.name;
+                    }
+                  });
+              },
             ),
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 16.0),
