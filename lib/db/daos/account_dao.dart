d592f2865d5738422f1da7c219c50202efb8e2e1
leandrofabianjr <leandrofabianjr@gmail.com>
2020-04-18 10:41:07 -0300

    Criado daos de conta e tipo de conta

---
+++ lib/db/daos/account_dao.dart
@@ -0,0 +1,42 @@
+import 'package:finapp/helpers/date_helper.dart';
+import 'package:finapp/models/account.dart';
+
+import 'dao.dart';
+
+class AccountDao extends Dao<Account> {
+  @override
+  String tblName = 'account_type';
+  @override
+  String colPk = 'id';
+
+  String colId = 'id';
+  String colName = 'name';
+  String colDescription = 'description';
+  String colIdAccountType = 'id_account_type';
+  String colCreatedAt = 'created_at';
+  String colDeletedAt = 'deleted_at';
+
+  @override
+  Account toObj(Map<String, dynamic> row) {
+    return Account(
+      id: row[colId],
+      name: row[colName],
+      description: row[colDescription],
+      idAccountType: row[colIdAccountType],
+      createdAt: DateHelper.stringToDateTime(row[colCreatedAt]),
+      deletedAt: DateHelper.stringToDateTime(row[colDeletedAt]),
+    );
+  }
+
+  @override
+  Map<String, dynamic> toRow(Account obj) {
+    return {
+      colId: obj.id,
+      colName: obj.name,
+      colDescription: obj.description,
+      colIdAccountType: obj.idAccountType,
+      colCreatedAt: obj.createdAt.toString(),
+      colDeletedAt: obj.deletedAt.toString()
+    };
+  }
+}
---
+++ lib/db/daos/account_type_dao.dart
@@ -0,0 +1,39 @@
+import 'package:finapp/helpers/date_helper.dart';
+import 'package:finapp/models/account_type.dart';
+
+import 'dao.dart';
+
+class AccountTypeDao extends Dao<AccountType> {
+  @override
+  String tblName = 'account_type';
+  @override
+  String colPk = 'id';
+
+  String colId = 'id';
+  String colName = 'name';
+  String colDescription = 'description';
+  String colCreatedAt = 'created_at';
+  String colDeletedAt = 'deleted_at';
+
+  @override
+  AccountType toObj(Map<String, dynamic> row) {
+    return AccountType(
+      id: row[colId],
+      name: row[colName],
+      description: row[colDescription],
+      createdAt: DateHelper.stringToDateTime(row[colCreatedAt]),
+      deletedAt: DateHelper.stringToDateTime(row[colDeletedAt]),
+    );
+  }
+
+  @override
+  Map<String, dynamic> toRow(AccountType obj) {
+    return {
+      colId: obj.id,
+      colName: obj.name,
+      colDescription: obj.description,
+      colCreatedAt: obj.createdAt.toString(),
+      colDeletedAt: obj.deletedAt.toString()
+    };
+  }
+}
