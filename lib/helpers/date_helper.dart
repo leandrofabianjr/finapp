8eadd1c5bf845c03531f4561c36cec7301150ec7
leandrofabianjr <leandrofabianjr@gmail.com>
2020-04-18 10:19:59 -0300

    Adicionado condição para quando a data vier como string com valor 'null'

--- lib/helpers/date_helper.dart
+++ lib/helpers/date_helper.dart
@@ -1,6 +1,6 @@
 class DateHelper {
   static DateTime stringToDateTime(String str) {
-    if (str != null && str.isNotEmpty) {
+    if (str != null && str != 'null' && str.isNotEmpty) {
       return DateTime.parse(str);
     }
     return null;
